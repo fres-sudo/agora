19b964462dbd107c80524024ccacc0496f3536e6
fres-sudo <francescocalicchio@hotmail.com>
2026-01-07 11:54:12 +0100

    refactor(main): update main application entry point
    Update main.dart to remove unused imports and align with new
    application structure.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:agora/core/ui/theme.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
