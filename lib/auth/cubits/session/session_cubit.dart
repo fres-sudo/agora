b4fb06e0cde08c1c354713be1e520656aa7b2d71
fres-sudo <francescocalicchio@hotmail.com>
2026-01-07 11:54:12 +0100

    refactor(auth): update session cubit
    Update session cubit to work with new navigation structure.

--- lib/auth/cubits/session/session_cubit.dart
+++ lib/auth/cubits/session/session_cubit.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:agora/auth/repositories/auth_repository.dart';
-import 'package:agora/core/misc/result.dart';
 
 part 'session_state.dart';
 part 'session_state_utils.dart';
@@ -12,17 +11,9 @@ part 'session_state_utils.dart';
 part 'session_cubit.freezed.dart';
 
 class SessionCubit extends Cubit<SessionState> {
-  SessionCubit(this._authRepository) : super(const SessionState.initial()) {
-    // _authSubscription = _authRepository.userStream.listen((user) {
-    //   if (user != null) {
-    //     emit(SessionState.authenticated(user: user));
-    //   } else {
-    //     emit(const SessionState.unauthenticated());
-    //   }
-    // });
-  }
+  SessionCubit(AuthRepository authRepository) : super(const SessionState.initial());
 
-  final AuthRepository _authRepository;
+  // final AuthRepository _authRepository;
   // late final StreamSubscription<AppUser?> _authSubscription;
 
   // Future<void> signOut() async => await _authRepository.signOut().unwrapAsync();
